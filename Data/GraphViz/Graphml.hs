922b4150f6cfc8761cb9b5854dd7912cd32e6909
Philipp Sieweck <psi@informatik.uni-kiel.de>
2013-09-24 13:24:10 +0200

    Updated to xmlgen-0.6.*. Implemented newline conversion.

--- Data/GraphViz/Graphml.hs
+++ Data/GraphViz/Graphml.hs
@@ -1,26 +1,29 @@
 {-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE OverloadedStrings #-}
 module Data.GraphViz.Graphml where
 
-import Control.Monad.State
-import Data.Maybe
-import Data.Functor
-import Control.Arrow (first)
-import Data.GraphViz.Types
-import Data.GraphViz.Types.Canonical
-import Data.GraphViz.Attributes.Complete
-import qualified Data.Text.Lazy as T
-import qualified Data.GraphViz.Attributes.Colors as AC
-import Data.Monoid
-import Text.XML.Generator
+import           Control.Arrow (first)
+import           Control.Monad.State
 import qualified Data.ByteString as B
-import qualified Data.Colour.SRGB as SRGB
 import qualified Data.Colour as C
+import qualified Data.Colour.SRGB as SRGB
+import           Data.Functor
+import qualified Data.GraphViz.Attributes.Colors as AC
+import           Data.GraphViz.Attributes.Complete
 import qualified Data.GraphViz.Parsing as DP
-import qualified Data.Text.Lazy.IO as TIO
+import           Data.GraphViz.Types
+import           Data.GraphViz.Types.Canonical
 import qualified Data.Map as M
+import           Data.Maybe
+import           Data.Monoid
+import qualified Data.Text as TS
+import qualified Data.Text.Lazy as T
+import qualified Data.Text.Lazy.IO as TIO
+import           Text.XML.Generator
 
 
-convert :: DotGraph String -> Xml Doc
+convert :: DotGraph TS.Text -> Xml Doc
 convert dg = doc defaultDocInfo $ xelem "graphml" $
              xattr "xmlns" "http://graphml.graphdrawing.org/xmlns" <>
              xattr "xmlns:y" "http://www.yworks.com/xml/graphml" <#>
@@ -32,43 +35,46 @@ convert dg = doc defaultDocInfo $ xelem "graphml" $
                           xattr "yfiles.type" "edgegraphics") <>
              handleStmts (graphStatements dg)
 
-handleStmts :: DotStatements String -> Xml Elem
+handleStmts :: DotStatements TS.Text -> Xml Elem
 handleStmts DotStmts{..} =
-    xelem "graph" (xattr "edgedefault" "directed" <>
-                   xattr "id" "G" <#>
-                   mconcat (map (handleNode nmap) nodeStmts) <>
-                   mconcat (map handleOrphanNode (M.assocs orphans)) <>
-                   mconcat (map (handleEdge nmap) $
-                            zip [0..] edgeStmts))
+    let !xnodes = map (handleNode nmap) nodeStmts
+        !xorphans = map handleOrphanNode (M.assocs orphans)
+        !xedges = map (handleEdge nmap) (zip [0..] edgeStmts)
+    in
+     xelem "graph" (xattr "edgedefault" "directed" <>
+                    xattr "id" "G" <#>
+                    mconcat xnodes <>
+                    mconcat xorphans <>
+                    mconcat xedges)
   where
     nmap' = M.fromList $ map (first nodeID) $ zip nodeStmts [0..]
     nmap nn = case M.lookup nn nmap' of
-                Just nid -> "n" ++ show nid
-                Nothing -> "n" ++ show (orphans M.! nn)
-    orphans = M.fromList $ flip zip [length nodeStmts ..] $
-              filter (not . flip M.member nmap') $
-              foldr (\s a -> fromNode s:toNode s:a) [] edgeStmts
+                Just nid -> "n" <> TS.pack (show nid)
+                Nothing -> "n" <> TS.pack (show (orphans M.! nn))
+    !orphans = M.fromList $ flip zip [length nodeStmts ..] $
+               filter (not . flip M.member nmap') $
+               foldr (\s a -> fromNode s:toNode s:a) [] edgeStmts
 
-handleNode :: (String -> String) -> DotNode String -> Xml Elem
+handleNode :: (TS.Text -> TS.Text) -> DotNode TS.Text -> Xml Elem
 handleNode nodeName DotNode{nodeID=nn,nodeAttributes=attrs} =
     xelem "node" (xattr "id" (nodeName nn) <#>
                   xelem "data" (xattr "key" "d6" <#>
                                 yNodeToXml (handleNodeAttrs nn attrs)))
 
-handleOrphanNode :: (String, Int) -> Xml Elem
-handleOrphanNode (nn, nid) = xelem "node" (xattr "id" ("n" ++ show nid) <#>
+handleOrphanNode :: (TS.Text, Int) -> Xml Elem
+handleOrphanNode (nn, nid) = xelem "node" (xattr "id" ("n" <> (TS.pack $ show nid)) <#>
                                            xelem "data" (xattr "key" "d6" <#>
                                                          yNodeToXml oattrs))
   where
     oattrs = defaultYNode { yLabel = nn }
 
-data YNode = YNode { yBgColor :: String
-                   , yFgColor :: String
-                   , yBorderColor :: String
-                   , yBorderWidth :: Double
-                   , yBorderStyle :: String
-                   , yLabel   :: String
-                   , yShape :: String
+data YNode = YNode { yBgColor :: !TS.Text
+                   , yFgColor :: !TS.Text
+                   , yBorderColor :: !TS.Text
+                   , yBorderWidth :: !Double
+                   , yBorderStyle :: !TS.Text
+                   , yLabel   :: !TS.Text
+                   , yShape :: !TS.Text
                    }
 
 defaultYNode :: YNode
@@ -87,27 +93,27 @@ yNodeToXml YNode{..} =
     [xelem "y:Fill" (xattr "color" yBgColor <> xattr "transparent" "false")
     ,xelem "y:BorderStyle" (xattr "color" yBorderColor <>
                             xattr "type" yBorderStyle <>
-                            xattr "width" (show yBorderWidth))
+                            xattr "width" (TS.pack $ show yBorderWidth))
     ,xelem "y:NodeLabel" (xattr "textColor" yFgColor <#>
-                          xtext yLabel)
+                          (xtext yLabel))
     ,xelem "y:Shape" (xattr "type" yShape)
     ]
 
-color2hex :: AC.Color -> String
-color2hex c = fromMaybe "#000000" $
+color2hex :: AC.Color -> TS.Text
+color2hex c = TS.pack $ fromMaybe "#000000" $
               (SRGB.sRGB24show . (`C.over` C.black)) <$> AC.toColour c
 
-wcolor2hex :: WeightedColor -> String
+wcolor2hex :: WeightedColor -> TS.Text
 wcolor2hex (WC c _) = color2hex c
 
-handleNodeAttrs :: String -> Attributes -> YNode
+handleNodeAttrs :: TS.Text -> Attributes -> YNode
 handleNodeAttrs defLbl = foldr go defaultYNode { yLabel = defLbl }
   where go attr s = case attr of
           Color (c:_) -> s { yBorderColor = wcolor2hex c }
           BgColor (c:_) -> s { yBgColor = wcolor2hex c }
           FillColor (c:_) -> s { yBgColor = wcolor2hex c }
           FontColor c -> s { yFgColor = color2hex c }
-          Label (StrLabel l) -> s { yLabel = T.unpack l }
+          Label (StrLabel l) -> s { yLabel = T.toStrict $ T.replace "\\n" "\n" l }
           Shape sh -> s { yShape = shapeName sh }
           Style (i:is) -> applyStyle i $ go (Style is) s
           _ -> s
@@ -133,24 +139,24 @@ handleNodeAttrs defLbl = foldr go defaultYNode { yLabel = defLbl }
           _ -> s
                                           
 
-handleEdge :: (String -> String) -> (Int, DotEdge String) -> Xml Elem
+handleEdge :: (TS.Text -> TS.Text) -> (Int, DotEdge TS.Text) -> Xml Elem
 handleEdge nodeName (eid, DotEdge{..}) =
     xelem "edge" $
-    xattr "id" ("e" ++ show eid) <>
+    xattr "id" ("e" <> TS.pack (show eid)) <>
     xattr "source" (nodeName fromNode) <>
     xattr "target" (nodeName toNode) <#>
     xelem "data" (xattr "key" "d10" <#>
                   yEdgeToXml (handleEdgeAttrs edgeAttributes))
 
 
-data YEdge = YEdge { yeWidth :: Double
-                   , yeStyle :: String
-                   , yeColor :: String
-                   , yeArrSrc :: Maybe String
-                   , yeArrDst :: Maybe String
-                   , yeDir    :: DirType
-                   , yeLabel :: Maybe String
-                   , yeLblColor :: String
+data YEdge = YEdge { yeWidth :: !Double
+                   , yeStyle :: !TS.Text
+                   , yeColor :: !TS.Text
+                   , yeArrSrc :: !(Maybe TS.Text)
+                   , yeArrDst :: !(Maybe TS.Text)
+                   , yeDir    :: !DirType
+                   , yeLabel :: !(Maybe TS.Text)
+                   , yeLblColor :: !TS.Text
                    }
 
 defaultYEdge :: YEdge
@@ -171,7 +177,7 @@ handleEdgeAttrs = foldr go defaultYEdge
           ArrowHead (AType ((_,sh):_)) -> s { yeArrDst = Just (arrShape sh) }
           ArrowTail (AType ((_,sh):_)) -> s { yeArrSrc = Just (arrShape sh) }
           Dir dir -> s { yeDir = dir }
-          Label (StrLabel l) -> s { yeLabel = Just (T.unpack l) }
+          Label (StrLabel l) -> s { yeLabel = Just (T.toStrict l) }
           FontColor c -> s { yeLblColor = color2hex c }
           Style (i:is) -> applyStyle i (go (Style is) s)
           _ -> s
@@ -201,7 +207,7 @@ yEdgeToXml YEdge{..} =
     xelem "y:BendStyle" (xattr "smoothed" "false") <>
     xelem "y:LineStyle" (xattr "color" yeColor <>
                          xattr "type" yeStyle <>
-                         xattr "width" (show yeWidth)) <>
+                         xattr "width" (TS.pack (show yeWidth))) <>
     case yeLabel of
       Just lbl -> xelem "y:EdgeLabel" $
                   xattr "textColor" yeLblColor <#>
@@ -217,5 +223,5 @@ yEdgeToXml YEdge{..} =
 convertDot :: FilePath -> IO ()
 convertDot fn = do
   mm <- TIO.readFile fn
-  let dg = DP.parseIt' mm :: DotGraph String
+  let dg = fmap T.toStrict (DP.parseIt' mm :: DotGraph T.Text)
   B.writeFile (fn ++ ".graphml") $ xrender (convert dg)
